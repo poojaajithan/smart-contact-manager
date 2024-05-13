670e0cbf6c1658a4cbe4e8425ef82a2d9ddf70f8
Pooja Ajithan <poojaajithan91@gmail.com>
2024-05-13 21:16:36 +0530

    Setting up Tailwind and Flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/main/resources/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  darkMode: "selector",
+}
+
